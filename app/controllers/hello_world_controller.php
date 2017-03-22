3f882b21e4fda7d0c2a7c1326d0a43c5e8d2ca63
Peppi <Nabscina@users.noreply.github.com>
2017-03-23 01:29:31 +0200

    uusia näkymiä

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,32 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        echo 'Tämä ei ole etusivu!';
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+    public static function sandbox() {
+
+        View::make('helloworld.html');
     }
-  }
+
+    public static function secret() {
+
+        echo 'You found a secret!';
+    }
+
+    public static function recipes_list() {
+        View::make('suunnitelmat/recipes_list.html');
+    }
+
+    public static function recipe_show() {
+        View::make('suunnitelmat/recipe_show.html');
+    }
+    
+        public static function recipe_edit() {
+        View::make('suunnitelmat/recipe_edit.html');
+    }
+
+}
--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,25 @@
 <?php
 
-  $routes->get('/', function() {
+$routes->get('/', function() {
     HelloWorldController::index();
-  });
+});
 
-  $routes->get('/hiekkalaatikko', function() {
+$routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
-  });
+});
+
+$routes->get('/supersecret', function() {
+    HelloWorldController::secret();
+});
+
+$routes->get('/recipes', function() {
+    HelloWorldController::recipes_list();
+});
+
+$routes->get('/recipes/1', function() {
+    HelloWorldController::recipe_show();
+});
+
+$routes->get('/recipes/1/edit', function() {
+    HelloWorldController::recipe_edit();
+});
